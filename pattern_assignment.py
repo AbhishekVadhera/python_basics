6d1e3cc0bf0765c59d6843ff520992ec4270fc25
Abhishek <abhishekvadhera03@gmail.com>
2023-08-11 17:24:28 +0530

    INTIAL COMMIT

---
+++ pattern_assignment.py
@@ -0,0 +1,28 @@
+for x in range(5, 0, -1):
+    for y in range(x, 0, -1):
+        print(y, end=' ')
+    print()
+
+for x in range(1, 6):
+    for y in range(x, 0, -1):
+        print(y, end=' ')
+    print()
+
+for x in range(1, 6):
+    for y in range(1, x + 1):
+        print(y, end=' ')
+    print()
+
+for x in range(1, 11):
+    for y in range(x, 0, -1):
+        print(y, end=' ')
+    print()
+
+print('-------------------------------------------------------------------------------')
+for x in range(10,0,-1):
+    for y in range(x,0,-1):
+        print(y,end=' ')
+    print()
+
+
+print('-----------------------------------------------------------------------------------------')
