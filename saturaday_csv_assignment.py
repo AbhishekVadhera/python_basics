033467e5582c5e026512f97479dcd125971e40f4
Abhishek <abhishekvadhera03@gmail.com>
2023-08-13 18:58:15 +0530

    INITIAL COMMIT

--- saturaday_csv_assignment.py
+++ saturaday_csv_assignment.py
@@ -5,6 +5,8 @@ with open('file.csv', 'r') as saturday_csv_assignment:
     conversion = list(read_file)
 
 checking_word = input('type your answer : ')
-for i, rows in enumerate(conversion):
+for rows in conversion:
     if checking_word not in rows:
-        print(conversion)
+        with open('File,csv','w') as saturday_csv_assignment:
+            result = csv.writer(saturday_csv_assignment)
+
